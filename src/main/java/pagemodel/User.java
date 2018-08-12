cd93ff74a833428443f22cdd4b08ccdacd5cd3e0
zhangpc <zhangpc@seehoo.cn>
2018-08-12 19:10:53 +0800

    first commit

---
+++ src/main/java/pagemodel/User.java
@@ -0,0 +1,52 @@
+package pagemodel;
+
+public class User {
+	String username;
+	String password;
+	String pic;
+	String age;
+	String userId;
+	String test1;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPic() {
+		return pic;
+	}
+	public void setPic(String pic) {
+		this.pic = pic;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+    public String getTest1() {
+        return test1;
+    }
+
+    public void setTest1(String test1) {
+        this.test1 = test1;
+    }
+}
